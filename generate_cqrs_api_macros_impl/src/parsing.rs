0283aff9e1542ae70b6b9709c282edb4c53a9d47
patmuk <patrick@mukherjee.de>
2024-12-10 10:12:50 +0100

    deactivates not used function

--- generate_cqrs_api_macros_impl/src/parsing.rs
+++ generate_cqrs_api_macros_impl/src/parsing.rs
@@ -1,6 +1,7 @@
 pub(crate) mod file_location_2_base_path;
 pub(crate) mod get_enum;
 pub(crate) mod get_struct_by_trait;
-pub(crate) mod get_use_statements;
+// not used, but kept if neede later.
+// pub(crate) mod get_use_statements;
 pub(crate) mod read_rust_files;
 pub(crate) mod type_2_ident;
